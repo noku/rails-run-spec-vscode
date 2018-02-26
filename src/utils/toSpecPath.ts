d5014fdc1232c60ec8e2ccda32690759f346a44f
Emiliano Leite <emiliano.leite@codus.com.br>
2018-02-26 20:06:19 -0300

    Use whole path when file is not inside 'app' folder. With this change, specs for files in unconventional folders (like 'lib') will also be found (as long as the spec path mirrors the impl. path).

--- src/utils/toSpecPath.ts
+++ src/utils/toSpecPath.ts
@@ -1,10 +1,13 @@
 export default function toSpecPath(filePath: string) {
     let [first, ...rest] = filePath.split('/');
+    let middle = rest.slice(0, rest.length - 1);
+    let filename = rest[rest.length - 1];
+
     if (filePath.match(/_spec\.rb/) || first === 'spec') {
         return filePath
+    } else if (first !== 'app') {
+        return ['spec', first, ...middle, filename.replace('.rb', '_spec.rb')].join('/');
     } else {
-        let middle = rest.slice(0, rest.length - 1);
-        let filename = rest[rest.length - 1];
         return ['spec', ...middle, filename.replace('.rb', '_spec.rb')].join('/');
     }
 }
