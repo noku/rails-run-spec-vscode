f88f1de741ea5f3413905ffb2238ce3c6ac02907
Tejas R. Mandke <tejas.mandke@gmail.com>
2018-03-29 11:17:44 -0700

    Add support for minitest (#10)

--- src/utils/toSpecPath.ts
+++ src/utils/toSpecPath.ts
@@ -1,10 +1,10 @@
-export default function toSpecPath(filePath: string) {
+export default function toSpecPath(filePath: string, pattern: string) {
     let [first, ...rest] = filePath.split('/');
-    if (filePath.match(/_spec\.rb/) || first === 'spec') {
+    if (filePath.indexOf(`_${pattern}.rb`) > -1 || first === pattern) {
         return filePath
     } else {
         let middle = rest.slice(0, rest.length - 1);
         let filename = rest[rest.length - 1];
-        return ['spec', ...middle, filename.replace('.rb', '_spec.rb')].join('/');
+        return [pattern, ...middle, filename.replace('.rb', `_${pattern}.rb`)].join('/');
     }
 }
