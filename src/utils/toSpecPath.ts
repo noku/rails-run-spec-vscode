07dff41c91dda8efd5ba8f72b9b253fab6d0983e
Nicholas Koh <nkohwl@gmail.com>
2018-09-23 19:12:33 +0800

    Add config to use forward slashes in path regardless of OS

--- src/utils/toSpecPath.ts
+++ src/utils/toSpecPath.ts
@@ -1,10 +1,16 @@
+import * as vscode from 'vscode';
+
 export default function toSpecPath(filePath: string, pattern: string) {
     let [first, ...rest] = filePath.split('/');
+    let specPath;
     if (filePath.indexOf(`_${pattern}.rb`) > -1 || first === pattern) {
-        return filePath
+        specPath = filePath
     } else {
         let middle = rest.slice(0, rest.length - 1);
         let filename = rest[rest.length - 1];
-        return [pattern, ...middle, filename.replace('.rb', `_${pattern}.rb`)].join('/');
+        specPath = [pattern, ...middle, filename.replace('.rb', `_${pattern}.rb`)].join('/');
     }
+
+    const useForwardSlashes = vscode.workspace.getConfiguration("ruby").get("specUseForwardSlashesInPaths"); 
+    return useForwardSlashes ? specPath.split('\\').join('/') : specPath ;
 }
