196ab6199c9148f5a3409d9f1b5e4c05a6c40050
HenriqueMorato <riquemorato@gmail.com>
2018-10-30 18:54:12 -0300

    Run All Spec Files Command (#13)

--- src/extension.ts
+++ src/extension.ts
@@ -15,6 +15,8 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
+
+    context.subscriptions.push(vscode.commands.registerCommand('extension.runAllSpecFiles', () => terminal.runSpecFile({path: "/"})));  
     context.subscriptions.push(vscode.commands.registerCommand('extension.runFileSpecs', () => terminal.runSpecFile({})));
     context.subscriptions.push(vscode.commands.registerCommand('extension.runSpecsFromMenu', (fileUri?: vscode.Uri) => {
         terminal.runSpecFile({path: fileUri.fsPath});
